1cf79b4ef6d8d8d3bdd0a16fe31f7dded439cefe
lingme <kid--l@hotmail.com>
2020-07-06 00:14:31 +0800

    update QuotesViewController timer

--- AntiMoment/QuotesViewController.swift
+++ AntiMoment/QuotesViewController.swift
@@ -11,9 +11,15 @@ import Cocoa
 class QuotesViewController: NSViewController {
     
     @IBOutlet var textLabel : NSTextField!
+    @IBOutlet var textYear : NSTextField!
+    @IBOutlet var textAge : NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+    }
+    
+    @objc func update() {
     }
 }
 
@@ -23,7 +29,7 @@ extension QuotesViewController {
     let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
     let identifier = NSStoryboard.SceneIdentifier("QuotesViewController")
     guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? QuotesViewController else {
-      fatalError("Why cant i find QuotesViewController? - Check Main.storyboard")
+      fatalError("")
     }
     return viewcontroller
   }
